cd2de509a40d63dc2016a02fd6dcf0376347448e
TiagoTReis <163140931+TiagoTReis@users.noreply.github.com>
2025-11-01 19:57:19 -0300

    feat: create Usuario.java

---
+++ padroes/strategy_observer_composite/observer/Usuario.java
@@ -0,0 +1,19 @@
+
+public class Usuario implements UsuarioObserver {
+    private String nome;
+    private NotificacaoStrategy estrategia; // utiliza Strategy
+
+    public Usuario(String nome, NotificacaoStrategy estrategia) {
+        this.nome = nome;
+        this.estrategia = estrategia;
+    }
+
+    public void setEstrategia(NotificacaoStrategy estrategia) {
+        this.estrategia = estrategia; // permite trocar a estratégia de notificação
+    }
+
+    @Override
+    public void atualizar(String mensagem) {
+        estrategia.notificar(nome + " recebeu: " + mensagem);
+    }
+}
