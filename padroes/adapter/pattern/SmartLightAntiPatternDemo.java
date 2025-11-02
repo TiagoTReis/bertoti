4131e402c5a3662a66da72d2cbef64c1bdbfc275
TiagoTReis <163140931+TiagoTReis@users.noreply.github.com>
2025-11-01 21:28:49 -0300

    feat: create SmartLightAntiPatternDemo.java

---
+++ padroes/adapter/pattern/SmartLightAntiPatternDemo.java
@@ -0,0 +1,8 @@
+public class SmartLightAntiPatternDemo {
+    public static void main(String[] args) {
+        System.out.println("=== DEMONSTRAÇÃO DO ANTI-PATTERN ADAPTER ===\n");
+
+        SmartHomeSystem system = new SmartHomeSystem();
+        system.connectOldLampDirectly();
+    }
+}
